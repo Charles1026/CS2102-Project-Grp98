602b72a8d3ff968bca2a018c59ce1a73ec548e78
Charles1026 <xiaozhehao@gmail.com>
2025-02-07 13:25:34 +0800

    added clean for passengers table and formatted license script

--- LicensePlateGenerator.py
+++ LicensePlateGenerator.py
@@ -5,32 +5,36 @@ import random
 MULTIPLES = [9, 4, 5, 4, 3, 2]
 LETTERS = ['A', 'Z', 'Y', 'X', 'U', 'T', 'S', 'R', 'P', 'M', 'L', 'K', 'J', 'H', 'G', 'E', 'D', 'C', 'B']
 
+def generateLicensePlate():
+  plateStr = "S"
+  plate = []
+  
+  # Letters
+  for j in range(2):
+    val = random.randint(1, 26)
+    plate.append(val)
+    plateStr += chr(64 + val)
+  
+  # Numbers
+  for j in range(4):
+    val = random.randint(0, 9)
+    plate.append(val)
+    plateStr += chr(48 + val)
+    
+  # Checksum
+  sum = 0
+  for i in range(6):
+    sum += plate[i] * MULTIPLES[i]
+    
+  plateStr += LETTERS[sum % 19]
+  
+  return plateStr
+
 if __name__ == "__main__":
   iterations = int(sys.argv[1])
   licenses = []
   for i in range(iterations):
-    plateStr = "S"
-    plate = []
-    
-    # Letters
-    for j in range(2):
-      val = random.randint(1, 26)
-      plate.append(val)
-      plateStr += chr(64 + val)
-    
-    # Numbers
-    for j in range(4):
-      val = random.randint(0, 9)
-      plate.append(val)
-      plateStr += chr(48 + val)
-      
-    # Checksum
-    sum = 0
-    for i in range(6):
-      sum += plate[i] * MULTIPLES[i]
-      
-    plateStr += LETTERS[sum % 19]
-    licenses.append(plateStr)
+    licenses.append(generateLicensePlate())
     
       
   with open('./license_plates.csv', 'w') as csvfile:
