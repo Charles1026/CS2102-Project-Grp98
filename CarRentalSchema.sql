b245773c4b6c7a6a2e8361dce833554c60e40b76
Charles1026 <xiaozhehao@gmail.com>
2025-02-07 13:42:24 +0800

    added sample data, minor bug fixes to schema and remove check with subquery

--- CarRentalSchema.sql
+++ CarRentalSchema.sql
@@ -29,12 +29,12 @@ CREATE TABLE IF NOT EXISTS customer (
   first_name VARCHAR(255),
   last_name VARCHAR(255),
   date_of_birth DATE NOT NULL,
-  prefered_make_id INTEGER, -- can be null
+  preferred_make_id INTEGER, -- can be null
 
   -- Logic
   CHECK (nric ~ '^[STFGM]\d{7}[A-Z]$'), -- regex for nric
   CHECK (first_name IS NOT NULL OR last_name IS NOT NULL), -- ensure that customer has at least 1 name field populated
-  FOREIGN KEY (prefered_make_id) REFERENCES make(make_id) ON UPDATE NO ACTION ON DELETE CASCADE
+  FOREIGN KEY (preferred_make_id) REFERENCES make(make_id) ON UPDATE NO ACTION ON DELETE CASCADE
 );
 
 
@@ -60,7 +60,7 @@ CREATE TABLE IF NOT EXISTS rental (
   EXCLUDE USING gist
     ( int4range (car_id, car_id, '[]') WITH &&, -- hack to avoid installing btree_gist
       daterange(start_date, end_date, '[]') WITH &&
-    ),
+    )
   -- CHECK (ARRAY_LENGTH(passengers, 1) > 0) -- check that there is at least 1 passenger
   -- CHECK (ARRAY_LENGTH(passengers, 1) <= (SELECT capacity FROM make WHERE make_id = (SELECT make_id FROM car WHERE car_id = rental.car_id)))
 );
@@ -71,7 +71,8 @@ CREATE TABLE IF NOT EXISTS passenger (
   customer_id INTEGER NOT NULL,
   has_driving_license BOOLEAN NOT NULL DEFAULT FALSE,
   FOREIGN KEY (rental_id) REFERENCES rental(rental_id) ON DELETE CASCADE,
-  FOREIGN KEY (customer_id) REFERENCES customer(customer_id) ON DELETE CASCADE,
+  FOREIGN KEY (customer_id) REFERENCES customer(customer_id) ON DELETE CASCADE
+/*,
   CONSTRAINT no_overlapping_passenger_rentals
     EXCLUDE USING gist (
       customer_id WITH =,
@@ -81,5 +82,6 @@ CREATE TABLE IF NOT EXISTS passenger (
         '[]'
       ) WITH &&
     )
+*/
 );
 
