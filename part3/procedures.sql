ce1042d55837223af03667dfd611fc7a4a128328
Charles1026 <xiaozhehao@gmail.com>
2025-04-02 16:47:32 +0800

    modify procedure to use sql

--- part3/procedures.sql
+++ part3/procedures.sql
@@ -9,12 +9,10 @@ $$ LANGUAGE sql;
 CREATE OR REPLACE PROCEDURE rent_group (IN car VARCHAR(8), IN customer CHAR(9),
     IN start_date DATE, IN end_date DATE , IN passenger1 CHAR(9), 
     IN passenger2 CHAR(9), IN passenger3 CHAR(9), IN passenger4 CHAR(9)) AS $$
-  BEGIN
   INSERT INTO rent VALUES (customer, car, start_date, end_date);
   INSERT INTO ride VALUES (car, start_date, end_date, customer);
   INSERT INTO ride VALUES (car, start_date, end_date, passenger2);
   INSERT INTO ride VALUES (car, start_date, end_date, passenger3);
   INSERT INTO ride VALUES (car, start_date, end_date, passenger4);
 
-  END;
-$$ LANGUAGE plpgsql;
+$$ LANGUAGE sql;
