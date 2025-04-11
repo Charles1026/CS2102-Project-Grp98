63be6372e757992634f852ed9169936a5d8dc883
Charles1026 <xiaozhehao@gmail.com>
2025-04-11 13:55:57 +0800

    force prodecures to defer constraint checks

--- part3/procedures.sql
+++ part3/procedures.sql
@@ -1,6 +1,7 @@
 -- Qb.1 Procedure for solo renter
 CREATE OR REPLACE PROCEDURE rent_solo (IN car VARCHAR(8), IN customer CHAR(9),
     IN start_date DATE, IN end_date DATE) AS $$
+  SET CONSTRAINTS ALL DEFERRED;
   INSERT INTO rent VALUES (customer, car, start_date, end_date);
   INSERT INTO ride VALUES (car, start_date, end_date, customer);
 $$ LANGUAGE sql;
@@ -9,6 +10,7 @@ $$ LANGUAGE sql;
 CREATE OR REPLACE PROCEDURE rent_group (IN car VARCHAR(8), IN customer CHAR(9),
     IN start_date DATE, IN end_date DATE , IN passenger1 CHAR(9), 
     IN passenger2 CHAR(9), IN passenger3 CHAR(9), IN passenger4 CHAR(9)) AS $$
+  SET CONSTRAINTS ALL DEFERRED;
   INSERT INTO rent VALUES (customer, car, start_date, end_date);
   INSERT INTO ride VALUES (car, start_date, end_date, passenger1);
   INSERT INTO ride VALUES (car, start_date, end_date, passenger2);
